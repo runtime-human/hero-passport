953dba1b9b28e7a578bf279e6e184d4bada1259a
MrFr3di <ynagi-wm@mail.ru>
2026-08-13 17:25:12 +0500

    test: add architecture test smoke

---
+++ tests/HeroPassport.Architecture.Tests/SmokeTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace HeroPassport.Architecture.Tests;
+
+public sealed class SmokeTests
+{
+    [Fact]
+    public void TestAssemblyLoads()
+    {
+        Assert.True(true);
+    }
+}
