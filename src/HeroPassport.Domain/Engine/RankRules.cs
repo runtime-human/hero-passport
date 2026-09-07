4052c84f27856d3be9b5de61614797e84499bb52
MrFr3di <ynagi-wm@mail.ru>
2026-09-07 13:59:17 +0500

    refactor(domain): introduce rank authority surface

---
+++ src/HeroPassport.Domain/Engine/RankRules.cs
@@ -0,0 +1,9 @@
+namespace HeroPassport.Domain.Engine;
+
+public static class RankRules
+{
+    public const string RuleVersion = "rank/1.0.0";
+
+    public static string Key(int heroLevel, string ruleVersion) =>
+        throw new NotImplementedException();
+}
