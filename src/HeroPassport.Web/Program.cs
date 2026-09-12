f721d152140f3506c9aa85c797ba9da31d516ddd
MrFr3di <ynagi-wm@mail.ru>
2026-09-12 22:20:08 +0500

    fix(web): enforce loopback-only Kestrel binding

--- src/HeroPassport.Web/Program.cs
+++ src/HeroPassport.Web/Program.cs
@@ -1,6 +1,11 @@
+using System.Net;
 using HeroPassport.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Listen(IPAddress.Loopback, 0);
+});
 builder.Services.AddRazorComponents();
 
 var app = builder.Build();
